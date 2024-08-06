4d92801d801df761a37adb189f844c297178d717
Artur Yesaian <artesayan2@gmail.com>
2024-08-06 15:10:58 +0200

    fix: solved the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let positiveNumber = Math.abs(n);
+    let numberStr = positiveNumber.toString();
+    let reversedStr = '';
+    
+    for (let i = numberStr.length - 1; i >= 0; i--) {
+      reversedStr += numberStr[i];
+    }
+    
+    let reversedNumber = parseInt(reversedStr, 10);
+    
+    return reversedNumber;
 }
